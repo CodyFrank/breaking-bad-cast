ba276cb668f08b9375462e539b27c832794bd85b
Cody Frank <cody.frank30@gmail.com>
2020-07-02 18:21:21 -0400

    sets up state and effect to fetch data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/ui/Header.js'
+import Axios from 'axios'
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() =>{
+    const fetchItems = async () => {
+      const result = await Axios('https://www.breakingbadapi.com/api/characters')
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [])
   return (
     <div className="container">
       <Header />
