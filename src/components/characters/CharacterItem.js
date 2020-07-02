a85c33bedd50e4b93ae5d3dadf8ab9ee78b68f09
Cody Frank <cody.frank30@gmail.com>
2020-07-02 18:35:50 -0400

    wires in Character Item component

---
+++ src/components/characters/CharacterItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CharacterItem = ( { item }) => {
+    return (
+        <div>
+            <h1>{item.name}</h1>
+        </div>
+    )
+}
+
+export default CharacterItem
